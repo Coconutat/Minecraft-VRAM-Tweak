fcdfedb9ba50a55a95884e4f4ec4477465e4e8af
Coconut <coconutat@protonmail.com>
2026-07-03 14:12:14 +0800

    ponytial: Remove some code

--- src/client/java/test/vram/tweak/client/VRAMTweakClient.java
+++ src/client/java/test/vram/tweak/client/VRAMTweakClient.java
@@ -2,13 +2,23 @@ package test.vram.tweak.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.network.chat.Component;
 import test.vram.tweak.VRAMTweak;
 import test.vram.tweak.command.VramTweakCommand;
+import test.vram.tweak.config.VRAMConfig;
+import test.vram.tweak.vram.VRAMGovernor;
 
 public class VRAMTweakClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        VRAMConfig.load(FabricLoader.getInstance().getConfigDir());
+        var cfg = VRAMConfig.getInstance();
+        VRAMTweak.LOGGER.info("VRAM optimizer. enabled={}, shadowCap={}, downscale={}, budget={}",
+                cfg.vram.enabled, cfg.vram.shadowMapMaxSize,
+                cfg.vram.formatDownscale, cfg.vram.budgetTracking);
+        VRAMGovernor.initialize();
+
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) ->
                 VramTweakCommand.register(dispatcher));
         VRAMTweak.LOGGER.info(Component.translatable("vramtweak.client.registered").getString());
--- src/main/java/test/vram/tweak/VRAMTweak.java
+++ src/main/java/test/vram/tweak/VRAMTweak.java
@@ -1,27 +1,14 @@
 package test.vram.tweak;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import test.vram.tweak.config.VRAMConfig;
-import test.vram.tweak.vram.VRAMGovernor;
 
-public class VRAMTweak implements ModInitializer {
+// ponytail: no longer ModInitializer — mod is client-only, init lives in VRAMTweakClient
+public class VRAMTweak {
     public static final String MOD_ID = "vram-tweak";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-    @Override
-    public void onInitialize() {
-        VRAMConfig.load(FabricLoader.getInstance().getConfigDir());
-        var cfg = VRAMConfig.getInstance();
-        LOGGER.info("VRAM optimizer. enabled={}, shadowCap={}, downscale={}, budget={}",
-                cfg.vram.enabled, cfg.vram.shadowMapMaxSize,
-                cfg.vram.formatDownscale, cfg.vram.budgetTracking);
-        VRAMGovernor.initialize();
-    }
 
     public static VRAMConfig getConfig() {
         return VRAMConfig.getInstance();
